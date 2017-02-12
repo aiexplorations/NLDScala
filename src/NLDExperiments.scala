33a9fc0bb0431eb015d75328828bdea454516aea
rajesh.sampathkumar <rajesh.sampathkumar@gmail.com>
2017-02-12 12:25:35 +0530

    Initial commit

---
+++ src/NLDExperiments.scala
@@ -0,0 +1,11 @@
+/**
+  * Created by Rajesh Sampathkumar on 2/12/2017.
+  */
+object NLDExperiments {
+
+  def main(args:Array[String]): Unit ={
+
+    val fp = new fixedPoint(x => 4*x*(1-x) )
+  }
+
+}
